ac4f28009e3ebe468079f3affce28b19f71f5b3c
Amrit14feb <24dec051@lnmiit.ac.in>
2025-07-05 11:06:32 +0000

    basic1 done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1402
+let accountEmail = "amrit.mishra@.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+//accountId = 2
+accountEmail = "jk@.com"
+accountPassword = "15432"
+console.log([accountEmail,accountPassword,accountCity]);
+console.table([accountEmail,accountPassword,accountCity,accountState])

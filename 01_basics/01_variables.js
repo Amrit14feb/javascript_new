8502cf2c077feb95eaa63b09b6fe375ae589d6a2
Amrit14feb <24dec051@lnmiit.ac.in>
2025-07-06 18:34:42 +0000

    jk

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,6 +4,8 @@ var accountPassword = "12345"
 accountCity = "jaipur"
 let accountState;
 //accountId = 2
+/*prefer  not to use var because of functional scope and block scope
+*/
 accountEmail = "jk@.com"
 accountPassword = "15432"
 console.log([accountEmail,accountPassword,accountCity]);

eff3da57e6aafc66ad50a4e581206e40bd8de964
Amrit14feb <24dec051@lnmiit.ac.in>
2025-07-08 10:36:22 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all js code as newer version 
+
+//alert(3 + 3)// we are using node js not browser
+
+let name = "amrit"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//BigInt
+//string=> ""
+//null=> standalone value
+//undefined => 
+
+//symbol=>unique
+
+console.log(typeof undefined)// undefined
+console.log(typeof null)//object
+
+console.log(typeof age)

9f0fcb66b0b4036a1ef58d449531716f36bb5b55
Amrit14feb <24dec051@lnmiit.ac.in>
2025-08-02 12:41:02 +0000

    objects2

--- 02_basics/04_objects2.js
+++ 02_basics/04_objects2.js
@@ -7,3 +7,25 @@ const tinderuser =  new Object()
 //tinderuser.name = "amrit"
 
 console.log(tinderuser);
+const regularUser = {
+    email : "some@gmail.com",
+    fullname : {
+        username: {
+            firstname: "amrit",
+            lastname : "mishra",
+        }
+    }
+}
+console.log(regularUser.fullname.username.firstname)
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a",4:"b"}
+
+//const obj3 = {obj1,obj2}
+
+
+const obj3  = Object.assign(obj1,obj2)//in this obj1 is target and obj2 is source....so to 
+//avoid this ....use blank curly brackets that would make new class of object and store 
+//the other two data into it.....({},obj1,obj2).or u can use spread operator.
+console.log(obj3)
+

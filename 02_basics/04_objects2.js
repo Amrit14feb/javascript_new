3ac4a420dfa3236f825d3b382fcbc16169873210
Amrit14feb <24dec051@lnmiit.ac.in>
2025-08-02 19:31:10 +0000

    objects

--- 02_basics/04_objects2.js
+++ 02_basics/04_objects2.js
@@ -29,3 +29,21 @@ const obj3  = Object.assign(obj1,obj2)//in this obj1 is target and obj2 is sourc
 //the other two data into it.....({},obj1,obj2).or u can use spread operator.
 console.log(obj3)
 
+//de-structuring objects.
+const course = {
+    coursename :"js",
+    price:"999",
+    courseInstructor : "amrit",
+}
+
+const{courseInstructor: instructor} = course
+console.log(instructor)
+
+//json api has format as below:
+//apis are either in the array format or object format
+//arrays and objects can be inside each other too
+{
+    "name" : "amrit",
+    "frriendname" : "rishi",
+    
+}

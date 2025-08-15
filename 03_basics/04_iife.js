4f4ab041065465f57153e654f461d09120b96431
Amrit14feb <24dec051@lnmiit.ac.in>
2025-08-15 09:58:39 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -2,9 +2,9 @@
 //IIFE is used to prevent the function from getting polluted by the global variables and 
 //execute the function on immediate basis.
 
-(function chai(){
-    console.log(`DB CONNECTED`);
-})(); //to end this line we need to put semicolon in the end of iife
+(function chai(ko){
+    console.log(`DB CONNECTED ${ko}`);
+})('mishra'); //to end this line we need to put semicolon in the end of iife
 
 ((name) => {
     console.log(`DB CONNECTED 2 ${name}`);
